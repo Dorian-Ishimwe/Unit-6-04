8efca9ab4f65e18aaa0b4c6d9ab4d4ec873eb8e6
DorianI <>
2021-05-05 15:43:22 +0000

    fixes told from the linter

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var a = inputA
-var b = inputB
+let a = 0
+let b = 0
 let h = 0
 let number = 0
 
 document.getElementById('button').addEventListener('click', Area)
 
 function Area () {
-  a = document.getElementById('inputA').value
-  b = document.getElementById('inputB').value
-  h = document.getElementById('inputH').value
+  a = document.getElementById('input-a').value
+  b = document.getElementById('input-b').value
+  h = document.getElementById('input-h').value
   number = parseInt(h)
 
   number = (a * b / 2) + h
